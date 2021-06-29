4688da0a8f1d8586528aaba03c7c6af7855cb002
I Am I <31686695+5ime@users.noreply.github.com>
2021-06-29 08:29:46 +0800

    Create bilidown.py

--- bilidown.py
+++ bilidown.py
@@ -13,39 +13,34 @@ https://www.bilibili.com/video/av*
 /*********************/
 """)
 
-cookie = input('''请粘贴你的哔哩哔哩cookies\n''')
-
+cookies = input('''请粘贴你的哔哩哔哩cookies\n''')
+cookie = {
+'Cookie': cookies
+}
 header = {
 'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1',
-'Cookie': cookie,
-'Origin': 'https://www.bilibili.com',
-'accept': 'application/json',
-'accept-encoding': 'gzip, deflate, br',
-'accept-language': 'en-US,en;q=0.8',
-'content-type': 'application/json',
 'Referer': 'https://www.bilibili.com'
 }
 s = requests.session()
-my_info = json.loads(s.get('http://api.bilibili.com/x/space/myinfo',allow_redirects=False,headers=header).text)
-
+my_info = json.loads(s.get('http://api.bilibili.com/x/space/myinfo',cookies=cookie).text)
 print("\n欢迎你！"+my_info['data']['name'])
 
-url = input("\n请粘贴哔哩哔哩视频链接\n")
+url = input("""\n请粘贴哔哩哔哩视频链接\n""")
 if 'https://b23.tv' in url: 
     loc = s.get(url,allow_redirects=False)
     url = loc.headers['location'] 
 if 'video/av' in url:
-    av = json.loads(s.get('https://api.bilibili.com/x/web-interface/archive/stat?aid='+url,allow_redirects=False,headers=header).text)
+    av = json.loads(s.get('https://api.bilibili.com/x/web-interface/archive/stat?aid='+url,headers=header,cookies=cookie).text)
     url = av['data']['bvid']
 video_id = re.findall("[\w.]*[\w:\-\+\%]",url)[3]
-vid = json.loads(s.get('https://api.bilibili.com/x/web-interface/view?bvid='+video_id,allow_redirects=False,headers=header).text)
+vid = json.loads(s.get('https://api.bilibili.com/x/web-interface/view?bvid='+video_id,headers=header,cookies=cookie).text)
 video = vid['data']['pages'][0]['cid']
 page = 0
 if vid['data']['videos'] > 1:
     page = int(input("这是一个多P视频,共%d集,请输入要下载第几集(例如：1)并回车："%vid['data']['videos']))
     page -= 1
     video = vid['data']['pages'][page]['cid']
-video_info = json.loads(s.get('https://api.bilibili.com/x/player/playurl?bvid='+video_id+'&cid='+str(video)+'&qn=80&otype=json',allow_redirects=False,headers=header).text)
+video_info = json.loads(s.get('https://api.bilibili.com/x/player/playurl?bvid='+video_id+'&cid='+str(video)+'&qn=80&otype=json',headers=header,cookies=cookie).text)
 video_name = vid['data']['title']
 video_url = video_info['data']['durl'][0]['url']
 
@@ -67,4 +62,5 @@ def Download(video_url,video_name,header):
                 now_jd = (data_count / content_size) * 100
                 print("\r视频下载进度：%d%%(%d/%d)" % (now_jd, data_count, content_size), end=" ")
             input('\n\n下载成功！下载的视频位于本程序同级目录中,按回车退出程序...')
+            
 Download(video_url,video_name,header)
